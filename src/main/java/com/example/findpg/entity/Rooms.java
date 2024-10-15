e95a3de7ed1e193f30fe613d52c0bd38e1c60873
MHKG <mehulkedia30@gmail.com>
2024-10-15 12:58:25 +0530

    Used Lombok

--- src/main/java/com/example/findpg/entity/Rooms.java
+++ src/main/java/com/example/findpg/entity/Rooms.java
@@ -2,6 +2,11 @@ package com.example.findpg.entity;
 
 import jakarta.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "rooms")
 public class Rooms {
@@ -14,36 +19,4 @@ public class Rooms {
     @Column private int pg_id;
     @Column private String room_type;
     @Column private float cost;
-
-    public int getRoom_id() {
-        return room_id;
-    }
-
-    public void setRoom_id(int room_id) {
-        this.room_id = room_id;
-    }
-
-    public int getPg_id() {
-        return pg_id;
-    }
-
-    public void setPg_id(int pg_id) {
-        this.pg_id = pg_id;
-    }
-
-    public String getRoom_type() {
-        return room_type;
-    }
-
-    public void setRoom_type(String room_type) {
-        this.room_type = room_type;
-    }
-
-    public float getCost() {
-        return cost;
-    }
-
-    public void setCost(float cost) {
-        this.cost = cost;
-    }
 }
