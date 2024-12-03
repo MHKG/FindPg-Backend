903da52115ff3b38fb69cc701439e2babaec7748
MHKG <mehulkedia30@gmail.com>
2024-12-03 15:39:20 +0530

    Change classpath position

--- src/main/java/com/example/findpg/config/PropertyReader.java
+++
@@ -1,8 +0,0 @@
-package com.example.findpg.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource({"classpath:/mysql.properties"})
-public class PropertyReader {}
