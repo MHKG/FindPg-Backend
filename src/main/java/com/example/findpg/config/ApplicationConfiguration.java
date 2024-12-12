b72ffa6172322c70ce68c927965499e90125a17b
MHKG <mehulkedia30@gmail.com>
2024-12-12 16:45:17 +0530

    Minor changes

--- src/main/java/com/example/findpg/config/ApplicationConfiguration.java
+++ src/main/java/com/example/findpg/config/ApplicationConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
-@Configuration
+@Configuratio
 public class ApplicationConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
